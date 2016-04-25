dbea7b2e0d4b2b7bc2319fd331864b69d51422b2
FeifeiyM <ayifeiyu@gmail.com>
2016-04-25 20:37:13 +0800

    add explanatory note

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = new koa()
 
 console.log('in koa')
 
-app.use(views(`${__dirname}/views`, {
+app.use(views(`${__dirname}/views`, {  //模板插件
 	ext: 'html',
 	noCache: true,
 	watch: true,
@@ -22,12 +22,13 @@ app.use(views(`${__dirname}/views`, {
 	}}
 }))
 
-app.use(mount('/static', server(`${__dirname}/public`)))
-app.use(logger())
-app.use(bodyParser())
-app.use(testRouter.routes())
+app.use(mount('/static', server(`${__dirname}/public`)))  //设置静态文件路径
+app.use(logger())  //日志
+app.use(bodyParser())  //参数解析
+app.use(testRouter.routes())  //路由
 	.use(testRouter.allowedMethods())
 
 app.listen(3000, () => console.log('server started 3000'))
 
 export default app
+
--- router/testRouter.js
+++ router/testRouter.js
@@ -4,23 +4,23 @@ import { readFilePromise, writeFilePromise } from '../application/filehandle'
 const router = new Router()
 console.log('in router')
 
-router.get('/', async (ctx, next) => {
+router.get('/', async (ctx, next) => {  //get请求，根路径
 	console.log('>>one')
 	await next()
 	console.log('<<one')
 	ctx.body = 'Hello World'
 })
 
-router.get('/start', async (ctx, next) => {
+router.get('/start', async (ctx, next) => {  //get请求，打开测试模板
 	console.log('in /start')
 
 	await ctx.render('template/start', {title: 'koa start'})
 })
 
-router.post('/start', async (ctx, next ) => {
+router.post('/start', async (ctx, next ) => {  //post请求
 	console.log('/start/post')
 
-	let reqParam = {
+	let reqParam = {  //请求参数提取
 		userName: ctx.request.body.user_name,
 		password: ctx.request.body.password
 	}
@@ -28,13 +28,13 @@ router.post('/start', async (ctx, next ) => {
 	console.dir(ctx.request.body)
 	let data = reqParam
 	await ctx.render('template/start', {title: 'koa start', data: data } )
-})
+})- 
 
 let respTest = () => {
 	return 'ok'
 }
 
-router.get('/proxy', async (ctx, next) => {
+router.get('/proxy', async (ctx, next) => {  //请求代理
 	console.log('in fetch')
 	let data = null
 	let url = 'http://interview.yunzao.cn/api/100/item/list'
@@ -45,23 +45,23 @@ router.get('/proxy', async (ctx, next) => {
 	ctx.body = resp1 + ' ' + resp2
 })
 
-router.get('/restful/userList', async (ctx, next) => {
+router.get('/restful/userList', async (ctx, next) => {  //get，查询列表
 	console.log('in /restful/userList')
 	let data = {}
 	let path = './resource/user.json'
-	let users = await readFilePromise(path)
+	let users = await readFilePromise(path)  //读取文件中数据
 	try {
 		users = JSON.parse(users)
 	} catch(err) {
 		console.log('data is not json format')
 	}
 	data.users = users
-	await ctx.render('restful/index', { title: 'RESTful', data: data })
+	await ctx.render('restful/index', { title: 'RESTful', data: data })  //渲染模板文件
 
 })
 
 router.post('/restful/addUser', async (ctx, next) => {
-	console.log( 'in /restful/adduser' )
+	console.log( 'in /restful/adduser' )  
 	let path = './resource/user.json'
 	let newUser = {  //获取数据
 		name: ctx.request.body.name,
@@ -69,12 +69,12 @@ router.post('/restful/addUser', async (ctx, next) => {
 		profession: ctx.request.body.profession
 	}
 
-	let users = await readFilePromise(path)
+	let users = await readFilePromise(path)  
 	try {
 		users = JSON.parse(users.trim())
 		newUser.id = users[users.length -1].id + 1
 		users.push( newUser )
-		await writeFilePromise(path, users)
+		await writeFilePromise(path, users)  //往文件中写入数据
 	} catch(err) {
 		console.error(err)
 	}
@@ -86,7 +86,7 @@ router.post('/restful/addUser', async (ctx, next) => {
 router.delete('/restful/deleteUser/:id', async (ctx, next) => {
 	console.log('/restful/deleteUser/:id')
 	let path = './resource/user.json'
-	let id = eval(ctx.params.id)
+	let id = eval(ctx.params.id)  //提取参数
 	let users = await readFilePromise(path)
 	console.log('id: ' + id)
 	try {
@@ -104,7 +104,7 @@ router.delete('/restful/deleteUser/:id', async (ctx, next) => {
 	}
 
 	ctx.status = 202
-	ctx.body = users
+	ctx.body = users  //配置响应数据
 
 })
 
